5b1bf42c11f37c9a5464d25a0d3caa809c876971
meri.davtyan <meri.davtyan@copa.team>
2024-08-07 07:13:39 +0000

    Final changes

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import folium
 from streamlit_folium import st_folium
 import plotly.graph_objects as go
@@ -20,7 +19,7 @@ def set_background(image_url):
         background-position: center;
         background-repeat: no-repeat;
         background-color: rgba(255, 255, 255, 0.8); /* Adjust the transparency here */
-        margin-top: -35px; /* Add space for the menu */
+        margin-top: -35px; 
     }}
     </style>
     """
@@ -50,7 +49,7 @@ st.sidebar.image(sidebar_logo, use_column_width=False, width=220)
 # Navigation menu
 menu_option = option_menu(
     None,
-    ["Home","Gender Distribution", "Timeseries", "Main Activities", "Main Products", "Roles", "Map"],
+    ["Home","Distribution", "Timeseries", "Main Activities", "Main Products", "Roles", "Map"],
     icons=["house","people", "clock", "activity", "box", "briefcase", "map"],
     menu_icon="cast",
     default_index=0,
@@ -92,12 +91,12 @@ st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs
 
 if menu_option == "Home":
     # Header
-    st.markdown('<h1 style="color:#e31f33;">Workplace Gender Roles</h1>', unsafe_allow_html=True)
-    set_background('https://images.pexels.com/photos/3861958/pexels-photo-3861958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+    # st.markdown('<h1 style="color:#e31f33;">Workplace Gender Roles</h1>', unsafe_allow_html=True)
+    set_background('https://i.pinimg.com/736x/10/31/1b/10311bd91a872378712d17ed79435f5a.jpg')
     # About Us section
     st.markdown("""
         <div style="
-            border: 2px solid red;
+            border: 2px;
             border-radius: 10px;
             padding: 20px;
             width: 70%;
@@ -124,7 +123,7 @@ if menu_option == "Home":
     # Contact Us section
     st.markdown("""
         <div style="
-            border: 2px solid red;
+            border: 2px;
             border-radius: 10px;
             padding: 20px;
             width: 70%;
@@ -138,18 +137,15 @@ if menu_option == "Home":
             <p>If you have any questions, feel free to reach out to us through the following channels:</p>
             <ul style="list-style-type: none; padding-left: 0;">
                 <li><i class="fab fa-github" style="color: #333; margin-right: 8px;"></i><a href="https://github.com/khatun1112/Spyur" target="_blank">GitHub</a></li>
-                <li><i class="fas fa-globe" style="color: #333; margin-right: 8px;"></i><a href="https://yourwebsite.com" target="_blank">Website</a></li>
-                <li><i class="fas fa-envelope" style="color: #333; margin-right: 8px;"></i><a href="mail.to.contact">Email Us</a></li>
+                <li><i class="fas fa-globe" style="color: #333; margin-right: 8px;"></i><a href="https://www.copa.team/" target="_blank">Website</a></li>
+                <li><i class="fas fa-envelope" style="color: #333; margin-right: 8px;"></i><a href="mailto:info@copa.team">Email Us</a></li>
             </ul>
         </div>
     """, unsafe_allow_html=True)
 
 
-
-if menu_option == "Gender Distribution":
-    st.subheader('Gender Distribution')
-    #https://images.pexels.com/photos/3757946/pexels-photo-3757946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
-    # Computing gender distribution
+if menu_option == "Distribution":
+    
     def compute_gender_distribution(df):
         aggregated_df = df.drop_duplicates(subset='url_id')
         gender_counts = aggregated_df['gender'].value_counts()
@@ -157,7 +153,7 @@ if menu_option == "Gender Distribution":
         
         male_percent = round((gender_counts.get('Male', 0) / total_count) * 100, 2)
         female_percent = round((gender_counts.get('Female', 0) / total_count) * 100, 2)
-        return gender_counts, male_percent, female_percent, 
+        return gender_counts, male_percent, female_percent
 
     gender_counts, male_percent, female_percent = compute_gender_distribution(filtered_df)
 
@@ -165,7 +161,6 @@ if menu_option == "Gender Distribution":
     male_counts = gender_counts.get('Male', 0)
     female_counts = gender_counts.get('Female', 0)
 
-    # Pie chart with hover information
     fig_pie = go.Figure(data=[go.Pie(labels=['Male', 'Female'], 
                                     values=[male_counts, female_counts], 
                                     customdata=[male_percent, female_percent],
@@ -174,57 +169,160 @@ if menu_option == "Gender Distribution":
                                     marker=dict(colors=[colors['Male'], colors['Female']]))])
 
     fig_pie.update_layout(
-        template='plotly_white'
+        template='plotly_white',
+        height=600,  
+        width=600,  
     )
 
-    col1, col2 = st.columns([1, 1])
+    col1, col2, col3 = st.columns([1, 1, 1])
 
     with col1:
+        st.markdown("          ")
+        st.subheader('Gender Distribution')
         st.plotly_chart(fig_pie)
 
     with col2:
-        st.markdown(f"""
-        ### Key Insights
-        - **Total Companies:** {gender_counts.sum()}
-        - **Male Executives:** {male_counts} ({male_percent}%)
-        - **Female Executives:** {female_counts} ({female_percent}%)
-        """)
+        st.markdown("""
+        <style>
+            .insight-box {
+                border: 1px solid #ddd;
+                border-radius: 8px;
+                padding: 15px;
+                margin-bottom: 20px;
+                background-color: #f9f9f9;
+            }
+            .insight-title {
+                font-size: 15px;
+                font-weight: bold;
+                margin-bottom: 10px;
+            }
+            .insight-list {
+                margin: 0;
+                padding: 0;
+                list-style-type: none;
+            }
+            .insight-list li {
+                margin-bottom: 8px;
+            }
+            .insight-list li::before {
+                content: '•';
+                color: #007bff;
+                font-weight: bold;
+                display: inline-block;
+                width: 1em;
+                margin-left: -1em;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+
+        # Key Insights Column
+        with st.expander("## Key Insights", expanded=True):
+            st.markdown('<div class="insight-title">Main Info:</div>', unsafe_allow_html=True)
+            st.markdown(f"<p>Total Companies: {gender_counts.sum()}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p>Male Executives: {male_counts} ({male_percent}%)</p>", unsafe_allow_html=True)
+            st.markdown(f"<p>Female Executives: {female_counts} ({female_percent}%)</p>", unsafe_allow_html=True)
+            st.markdown("<p>The Highest Pct of Female Executives Based on Company Size: Up to 15</p>", unsafe_allow_html=True)
+            st.markdown("<p>The Lowest Pct of Female Executives Based on Company Size: From 1001 to 2000</p>", unsafe_allow_html=True)
+            st.markdown("<p>The Highest Pct of Female Executives Based on Form of Ownership: State</p>", unsafe_allow_html=True)
+            st.markdown("<p>The Lowest Pct of Female Executives Based on Form of Ownership: Non-governmental</p>", unsafe_allow_html=True)
+            st.markdown("<p>The Pct of Female Executives in Capital City: Yerevan (33%)</p>", unsafe_allow_html=True)
+            st.markdown("<p>State with the Highest Pct of Female Executives: Aragatsotn (47%)</p>", unsafe_allow_html=True)
+            st.markdown("<p>State with the Lowest Pct of Female Executives: Vayoc Dzor (30%)</p>", unsafe_allow_html=True)
+            st.markdown('</div>', unsafe_allow_html=True)
+    with col3:
+        # Top Activities Column
+        st.markdown("          ")
+        with st.expander("Activities and Products", expanded=True):
+            st.markdown('<div class="insight-title">Top 3 Activities for Women:</div>', unsafe_allow_html=True)
+            st.markdown("""
+            <ul class="insight-list">
+                <li>Retail and E-commerce (13.58%)</li>
+                <li>Hospitality and Tourism (10.52%)</li>
+                <li>Education and Training Services (9.53%)</li>
+            </ul>
+            """, unsafe_allow_html=True)
+            
+            st.markdown('<div class="insight-title">Top 3 Activities for Men:</div>', unsafe_allow_html=True)
+            st.markdown("""
+            <ul class="insight-list">
+                <li>Manufacturing (15.54%)</li>
+                <li>Retail and E-commerce (13.43%)</li>
+                <li>Construction and Real Estate (10.35%)</li>
+            </ul>
+            """, unsafe_allow_html=True)
+            st.markdown('<div class="insight-title">Top 3 Products for Women:</div>', unsafe_allow_html=True)
+            st.markdown("""
+            <ul class="insight-list">
+                <li>Manufactoring and Industrial Equipment (10.54%)</li>
+                <li>Retail and Consumer Goods (9.68%)</li>
+                <li>Hospitality and Tourism (9.28%)</li>
+            </ul>
+            """, unsafe_allow_html=True)
+            st.markdown('</div>', unsafe_allow_html=True)
+            st.markdown('<div class="insight-title">Top 3 Products for Men:</div>', unsafe_allow_html=True)
+            st.markdown("""
+            <ul class="insight-list">
+                <li>Manufactoring and Industrial Equipment (19.55%)</li>
+                <li>Construction and Infrastructure (8.86%)</li>
+                <li>Retail and Consumer Goods (7.77%)</li>
+            </ul>
+            """, unsafe_allow_html=True)
+            st.markdown('</div>', unsafe_allow_html=True)
+
 
         
-
-
 if menu_option == "Timeseries":
     st.subheader('Timeseries')
     st.markdown('<p style="color:gray; font-size:12px;">This timeseries is based on founding year for companies who mentioned the date</p>', unsafe_allow_html=True)
 
-    # Filter by year established, remove rows with year_established as 0
-    filtered_df = final_df[final_df['year_established'] != 1990]
-    
+    filtered_df = final_df[final_df['year_established'] >= 1991]
+
     max_year = int(filtered_df['year_established'].max())
-    min_year = 1832
+    min_year = 1991
     selected_year_range = st.slider('Select Year Range', min_value=min_year, max_value=max_year, value=(min_year, max_year), step=1)
-    
-    # Further filter data based on selected year range
+
     filtered_df = filtered_df[(filtered_df['year_established'] >= selected_year_range[0]) & 
                               (filtered_df['year_established'] <= selected_year_range[1])]
+    
+    filtered_df = filtered_df[filtered_df['cluster'] != 'N/A']
+    clusters = filtered_df['cluster'].unique()
+    clusters_with_all = ['All'] + list(clusters)
+    
+    col1, col2 = st.columns([3, 1])  
+    
+    with col2:
+        st.markdown("###           ")
+        st.markdown("###           ")
+        selected_clusters = st.selectbox('Select Clusters', options=clusters_with_all)
+    
+    with col1:
+        if selected_clusters == 'All':
+             pass
+        else:
+            filtered_df = filtered_df[filtered_df['cluster'] == selected_clusters]
 
-    # Line plot for executive counts over years
-    fig_line = go.Figure()
-    aggregated_df = filtered_df.drop_duplicates(subset='url_id')
-    exec_counts = aggregated_df.groupby('year_established')['gender'].value_counts().unstack().fillna(0)
 
-    colors = {'Female': '#e22e1f', 'Male': '#4788c8'}
-    for gender in exec_counts.columns:
-        fig_line.add_trace(go.Scatter(x=exec_counts.index, y=exec_counts[gender], mode='lines+markers', name=gender, line=dict(color=colors.get(gender, '#000000'))))
+        # Line plot for executive percentages over years
+        fig_line = go.Figure()
+        aggregated_df = filtered_df.drop_duplicates(subset='url_id')
+        exec_counts = aggregated_df.groupby('year_established')['gender'].value_counts().unstack().fillna(0)
 
-    fig_line.update_layout(
-        title='Executive Counts Based On Year Established',
-        xaxis_title='Year Established',
-        yaxis_title='Count',
-        legend_title='Gender',
-        template='plotly_white'
-    )
-    st.plotly_chart(fig_line)
+        exec_totals = exec_counts.sum(axis=1)
+        exec_pct = exec_counts.div(exec_totals, axis=0) * 100
+
+        colors = {'Female': '#e22e1f', 'Male': '#4788c8'}
+        for gender in exec_pct.columns:
+            fig_line.add_trace(go.Scatter(x=exec_pct.index, y=exec_pct[gender], mode='lines+markers', name=gender, line=dict(color=colors.get(gender, '#000000'))))
+
+        fig_line.update_layout(
+            title='Executive Percentages Based On Year Established',
+            xaxis_title='Year Established',
+            yaxis_title='Percentage (%)',
+            legend_title='Gender',
+            template='plotly_white'
+        )
+        st.plotly_chart(fig_line)
+
 
 elif menu_option == "Main Activities":
     st.subheader('Main Activities')
